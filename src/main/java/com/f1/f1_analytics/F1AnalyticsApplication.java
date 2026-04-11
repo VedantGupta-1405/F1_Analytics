fbac5f243e275bedf6b1dc982a1046f368311e44
vedant <vedantgupta1405@gmail.com>
2026-04-11 16:43:31 +0530

    Initial Spring Boot setup + DB connection

---
+++ src/main/java/com/f1/f1_analytics/F1AnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.f1.f1_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1AnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1AnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f1/f1_analytics/F1AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1.f1_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
